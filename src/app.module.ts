90d8327b8e684e683b9bba1d1cf3bc48a6f6e033
Diego Saravia <diego.saraviazaldana@gmail.com>
2025-04-06 23:29:57 -0600

    Adding Dashboard module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from './providers/prisma/prisma.service';
+import { DashboardModule } from './modules/dashboard/dashboard.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    DashboardModule,
   ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
